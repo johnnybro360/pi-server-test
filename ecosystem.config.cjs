89202f241cb912ffd071d501b7af815c6a711ab0
Jonghun Park <abest82@gmail.com>
2026-06-27 01:09:38 +0800

    modify db and pm2 for pi optimisation

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,7 +5,9 @@ module.exports = {
         name: "api",
         script: "./server/dist/index.js",
         cwd: "/home/jon/dev/projects/pi-server-test",
-  
+        node_args: "--max-old-space-size=256",
+        max_memory_restart: "300M",
+
         env: {
           NODE_ENV: "production",
           API_PORT: 3001,
--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -2,6 +2,22 @@ import Database from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import "dotenv/config";
 
-const sqlite = new Database(process.env.DB_FILE_NAME);
+const isProd = process.env.NODE_ENV === "production";
+const dbPath = process.env.DB_FILE_NAME;
 
-export const db = drizzle({ client: sqlite});
+if (!dbPath) throw new Error("DB_FILE_NAME is missing");
+
+const sqlite = new Database(dbPath);
+
+sqlite.pragma("journal_mode = WAL");
+sqlite.pragma("foreign_keys = ON");
+
+if (isProd) {
+  // WAL mode ON (IMPORTANT)
+  sqlite.pragma("synchronous = NORMAL");
+  sqlite.pragma("temp_store = MEMORY");
+  sqlite.pragma("cache_size = 10000");
+  sqlite.pragma("mmap_size = 134217728"); // 128MB safer default
+}
+
+export const db = drizzle({ client: sqlite });
