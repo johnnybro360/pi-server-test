acfaf3f751b25822ca5ba65ac92d685c7473e09e
Jonghun Park <abest82@gmail.com>
2026-06-27 00:53:40 +0800

    modify directory in ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,12 +4,12 @@ module.exports = {
       {
         name: "api",
         script: "./server/dist/index.js",
-        cwd: "/home/pi/dev/projects/pi-server-test",
+        cwd: "/home/jon/dev/projects/pi-server-test",
   
         env: {
           NODE_ENV: "production",
           API_PORT: 3001,
-          DB_FILE_NAME: "/home/pi/dev/projects/pi-server-test/db.sqlite",
+          DB_FILE_NAME: "/home/jon/dev/projects/pi-server-test/db.sqlite",
         },
       },
     ],
